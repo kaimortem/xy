5fbb07dc7bf6754b2eafd9feeed03164b301dc5e
Lytecyde <mik.seljamaa.nagaoka@gmail.com>
2020-05-22 11:43:05 +0300

    added restart by mouseclick for the game

--- src/Board.java
+++ src/Board.java
@@ -7,7 +7,6 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class Board extends JPanel implements ActionListener {
-
     private Timer timer;
     private SpaceShip spaceship;
     private List<Alien> aliens;
@@ -18,55 +17,53 @@ public class Board extends JPanel implements ActionListener {
     private GameOver gameOver = new GameOver();
     private Constants constants = new Constants();
     private Point initial = new Point(Constants.ICRAFT_X, Constants.ICRAFT_Y);
+    private Button restart = new Button("Restart");
 
     public Board() {
-
         initBoard();
     }
 
-    private void initBoard() {
-
+    protected void initBoard() {
         setFocusable(true);
         setBackground(Color.BLACK);
         GameStatus.inGame = true;
         setPreferredSize(new Dimension(constants.B_WIDTH, constants.B_HEIGHT));
-
         spaceship = new SpaceShip(initial);
         addKeyListener(new SteeringAdaptor(spaceship));
-
         initAliens();
         collision = new Collision(aliens, spaceship);
-
         timer = new Timer(constants.DELAY, this);
         timer.start();
     }
 
     private void addAlienToBoard (int[] point){
-
         Point alienLocation = new Point(point[0], point[1]);
         aliens.add(new Alien(alienLocation));
     }
 
     public void initAliens() {
-
         aliens = new ArrayList<>();
-
         for (int[] point : constants.alienPositons) { addAlienToBoard(point); }
+    }
+
+    private void endGameScreen (Graphics g) {
+        gameOver.draw(g);
+        restartButton();
+    }
+
+    private void restartButton() {
+        addMouseListener(new ShooterMouseListener(this));
     }
 
     @Override
     public void paintComponent(Graphics g) {
-
         super.paintComponent(g);
-
-        if(GameStatus.isGameOver()) { gameOver.draw(g); }
+        if(GameStatus.isGameOver()) { endGameScreen(g);}
         if(GameStatus.inGame) { drawObjects(g); };
-
         Toolkit.getDefaultToolkit().sync();
     }
 
     private void drawGameObject(Graphics g, GamePiece gamePiece) {
-
         if (gamePiece.isVisible())
         {
             g.drawImage(
@@ -78,7 +75,6 @@ public class Board extends JPanel implements ActionListener {
     }
 
     private void drawObjects(Graphics g) {
-
         drawGameObject(g, spaceship);
         List<Missile> ms = spaceship.getMissiles();
         for (Missile missile : ms) { drawGameObject(g, missile); }
